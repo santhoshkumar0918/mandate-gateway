1028b44669bc33cabb8bc37dcc7838a12fe92c0d
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2026-09-02 21:21:37 +0530

    razorpay-client: remove invalid capture_payment endpoint

--- gateway/crates/razorpay-client/src/client.rs
+++ gateway/crates/razorpay-client/src/client.rs
@@ -56,40 +56,6 @@ impl RazorpayClient {
         }
     }
 
-    pub async fn capture_payment(
-        &self,
-        order_id: &str,
-        amount: i64,
-        currency: &str,
-        method: &str,
-    ) -> Result<Payment, RazorpayError> {
-        let body = serde_json::json!({
-            "amount": amount,
-            "currency": currency,
-            "method": method,
-            "order_id": order_id,
-        });
-
-        let resp = self
-            .client
-            .post(format!("{}/payments", BASE_URL))
-            .header("Authorization", &self.auth_header)
-            .header("Content-Type", "application/json")
-            .json(&body)
-            .send()
-            .await?;
-
-        if resp.status().is_success() {
-            Ok(resp.json().await?)
-        } else {
-            let err: ApiErrorResponse = resp.json().await?;
-            Err(RazorpayError::Api {
-                code: err.error.code,
-                description: err.error.description,
-            })
-        }
-    }
-
     pub async fn fetch_payment(&self, payment_id: &str) -> Result<Payment, RazorpayError> {
         let resp = self
             .client
